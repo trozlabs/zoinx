ada67c3d5af4e7ea75f026d132d8d4f8fa3e355b
Peter Tierney <schmaltzyman@gmail.com>
2023-12-08 11:32:40 -0600

    Made SelectInclude handle input as an array or as a string.

--- src/util/SelectInclude.js
+++ src/util/SelectInclude.js
@@ -2,19 +2,27 @@
 select: '[{"property":"object_type", "exclude":"false"}]'
 */
 
+const _ = require("lodash");
 module.exports = class SelectInclude {
     #selectInclude = [];
     #dbType = 'mongo';
     #select = '';
 
     constructor(req, dbType) {
-        if (req && req.query?.select) {
+        if (_.isArray(req)) {
+            if (!req[0]?.property) req = [];
+            this.#selectInclude = req;
+        }
+        else {
             this.#selectInclude = JSON.parse(req.query.select);
             if (typeof this.#selectInclude[0].exclude === 'string') {
                 if (this.#selectInclude[0].exclude.toLowerCase() === 'true') this.#selectInclude[0].exclude = true;
                 else this.#selectInclude[0].exclude = false;
             }
-            if (dbType) this.#dbType = dbType;
+        }
+
+        if (dbType) this.#dbType = dbType;
+        if (this.#selectInclude.length > 0) {
             this.parseSelect();
         }
     }
