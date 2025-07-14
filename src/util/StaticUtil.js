b575427260537db3670a10f820454bdde5b8ef08
Peter Tierney <schmaltzyman@gmail.com>
2025-07-14 01:28:50 -0500

    Added formatting bytes util functions

--- src/util/StaticUtil.js
+++ src/util/StaticUtil.js
@@ -225,4 +225,17 @@ module.exports = class UtilMethods {
     static generateRandomRangedInteger(min, max) {
         return Math.floor(min + Math.random()*(max - min + 1))
     }
+
+    static formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
+
 };
